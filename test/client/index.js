d4faa6f6cc104362bfd951d84147a907c45cf989
Shawn Freyssonnet-Inder <shawn@vigour.io>
2014-08-21 13:31:39 +0200

    - Added test for write from url on native. - Better notify method - Notify now waits for cordova ready (ios requires this)

--- test/client/index.js
+++ test/client/index.js
@@ -1,39 +1,61 @@
-function notify (prefix, msg) {
-    var str = prefix
-        , p
-    if (msg) {
-        str += ": " + msg
-    }
-    p = document.createElement('p')
-    p.appendChild(
-        document.createTextNode(str)
-    )
-    document.getElementById('messages').appendChild(p)
-}
 try {
+    var nativeUtil = require('../../lib/nativeUtil')
+
+    function notify () {
+        var args = arguments
+        nativeUtil.whenCordovaReady(function () {
+            p = document.createElement('p')
+            p.appendChild(
+                document.createTextNode([].slice.call(args).join(': '))
+            )
+            document.getElementById('messages').appendChild(p)
+        })
+    }
+
     var fs = require('../../')
         , filePath = 'out.txt'
         , fileContents = "File writer's block"
+        , url = "https://api.themoviedb.org/3/search/movie?api_key=1858b4fe727192fef95bf123deab5353&query=Mud"
+        , downloadTarget = 'out2.txt'
+
+
     fs.writeFile(filePath, fileContents, function (err) {
-      if (err) {
-        notify('FAIL write file', JSON.stringify(err))
-      } else {
-        notify('PASS write file')
+        if (err) {
+        notify('FAIL', 'write file', JSON.stringify(err))
+        } else {
+        notify('PASS', 'write file')
         fs.readFile(filePath, function (err, data) {
             if (err) {
-                notify('FAIL read file', JSON.stringify(err))
+                notify('FAIL', 'read file', JSON.stringify(err))
             } else {
-                notify('PASS read file', data)
+                notify('PASS', 'read file', JSON.stringify(data))
             }
         })
-      }
+        }
     })
 
-    fs.readFile('https://api.themoviedb.org/3/search/movie?api_key=1858b4fe727192fef95bf123deab5353&query=Mud', function (err, data, response) {
+    fs.writeFile(downloadTarget
+        , url
+        , function (err) {
+            if (err) {
+                notify('FAIL', 'testing write url', JSON.stringify(err))
+            } else {
+                notify('PASS', 'testing write url')
+                fs.readFile(downloadTarget, function (err, data) {
+                    if (err) {
+                        notify('FAIL', 'read downloaded', JSON.stringify(err))
+                    } else {
+                        notify('PASS', 'read downloaded', data)
+                    }
+                })
+            }
+        })
+
+    fs.readFile(url, function (err, data, response) {
       if (err) {
         notify('FAIL', 'testing read url', err)
       } else {
-        notify('PASS', 'testing read url', data/*.toString()*/)
+        notify('PASS', 'testing read url', JSON.stringify(data))
       }
     })
 } catch (e) {
