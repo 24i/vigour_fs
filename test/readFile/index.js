0abd3f748000da005e6526e79c285e2c4219a9c9
Shawn Inder <shawn@vigour.io>
2015-07-31 11:00:31 +0200

    more tests

--- test/readFile/index.js
+++ test/readFile/index.js
@@ -2,7 +2,7 @@ var chai = require('chai') // TODO Remove this when gaston allows it
 var expect = chai.expect	// TODO Remove this when gaston allows it
 var path = require('path')
 var fs = require('../../lib/server')
-
+var exec = require('child_process').exec
 var largeFilePath = path.join(__dirname, '..', 'data', 'large_file.txt')
 var tooBig = 1024 * 1024 * 1024
 var bigButFine = tooBig - 1
@@ -37,15 +37,49 @@ describe('fs.readFile', function () {
 		})
 	})
 
-	it("should fail on inexistant files", function (done) {
+	it("should fail with err.code === 'ENOENT' on inexistant files", function (done) {
 		var filename = path.join(__dirname, 'does_not_exist.txt')
-		fs.readFile(filename, function (err, data) {
+		fs.readFile(filename, 'raw', function (err, data) {
 			expect(err.code).to.equal("ENOENT")
+			expect(err.message.indexOf(filename) >= 0)
 			expect(data).not.to.exist
 			done()
 		})
 	})
-	  
+
+	it("should work on empty files", function (done) {
+		var filename = path.join(__dirname, '..', 'data', 'empty.txt')
+		fs.readFile(filename, function (err, data) {
+			expect(err).not.to.exist
+			expect(data).to.exist
+			done()
+		})
+	})
+
+	it("should work on empty files using utf8", function (done) {
+		var filename = path.join(__dirname, '..', 'data', 'empty.txt')
+		fs.readFile(filename, 'utf8', function (err, data) {
+			expect(err).not.to.exist
+			expect(data).to.equal('')
+			done()
+		})
+	})
+
+	it("should work on streams", function (done) {
+		var filename = path.join(__dirname, '..', 'data', 'one.txt')
+		var dataExpected = fs.readFileSync(filename, 'utf8')
+		var node = JSON.stringify(process.execPath)
+		var script = path.join(__dirname, '..', 'scripts', 'readFromStream.js')
+		var cmd = "cat " + filename + " | " + node + " " + script
+		exec(cmd, function (err, stdout, stderr) {
+			expect(err).not.to.exist
+			expect(stdout).to.equal(dataExpected)
+			expect(stderr).to.equal('')
+			done()
+		})
+	})
+	
+  
 	process.on('uncaughtException', function (err) {
 	  it('should not throw uncaughtException', function () {
 	  	expect(true).to.be.true
